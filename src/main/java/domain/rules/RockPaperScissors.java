201674b47bb7aab5c95a1d16abfbb647be5fa1cc
Redouanelh <redouan_school@outlook.com>
2020-03-28 14:53:37 +0100

    Made class RockPaperScissors.java shorter, made tests

--- src/main/java/domain/rules/RockPaperScissors.java
+++ src/main/java/domain/rules/RockPaperScissors.java
@@ -5,34 +5,47 @@ import domain.Result;
 import domain.Rules;
 
 public class RockPaperScissors implements Rules {
+
     @Override
     public Result decide(Move mine, Move theirs) {
-        switch (mine) {
-            case ROCK:
-                switch (theirs) {
-                    case SCISSORS:
-                        return Result.WIN;
-                    case PAPER:
-                        return Result.LOSE;
-                }
 
-            case PAPER:
-                switch (theirs) {
-                    case ROCK:
-                        return Result.WIN;
-                    case SCISSORS:
-                        return Result.LOSE;
-                }
-
-            case SCISSORS:
-                switch (theirs) {
-                    case PAPER:
-                        return Result.WIN;
-                    case ROCK:
-                        return Result.LOSE;
-                }
+        if (mine == theirs){
+            return Result.DRAW;
         }
 
-        return Result.DRAW;
+        switch (mine) {
+            case ROCK:
+                switch (theirs) { case SCISSORS: return Result.WIN; } break;
+
+            case PAPER:
+                switch (theirs) { case ROCK: return Result.WIN; } break;
+
+            case SCISSORS:
+                switch (theirs) { case PAPER: return Result.WIN; } break;
+        }
+
+        return Result.LOSE;
     }
+
+//    @Override
+//    public Result decide(Move mine, Move theirs) {
+//        Result result = Result.LOSE;
+//
+//        if (mine == theirs){
+//            return Result.DRAW;
+//        }
+//
+//        switch (mine) {
+//            case ROCK:
+//                if (theirs == Move.SCISSORS) { result = Result.WIN; break; }
+//
+//            case PAPER:
+//                if (theirs == Move.ROCK) { result = Result.WIN; break; }
+//
+//            case SCISSORS:
+//                if (theirs == Move.PAPER) { result = Result.WIN; break; }
+//        }
+//        return result;
+//    }
+
 }
